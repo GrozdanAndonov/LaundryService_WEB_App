1c432b59812ecbf341f61f4ddea8e03b9ed45ae1
GrodanAndonov <grozdan96@gmail.com>
2018-09-04 22:06:16 +0300

    fixed path to avatars

--- src/main/java/com/example/WebInitializer.java
+++ src/main/java/com/example/WebInitializer.java
@@ -33,7 +33,7 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
    		return multipartConfigElement;
    	}
 
-       public static final String LOCATION = "C:\\Users\\Anelia\\Desktop\\LaundryServiceUsersAvatars"; // Temporary location where files will be stored
+       public static final String LOCATION = "E:\\Diplomna\\LaundryServiceUsersAvatars"; // Temporary location where files will be stored
     
        private static final long MAX_FILE_SIZE = 5242880; // 5MB : Max file size.
                                                            // Beyond that size spring will throw exception.
